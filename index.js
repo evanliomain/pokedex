6f23e9da74babd7effe8693832ec1effa2e941ab
Evan Liomain <evan.liomain@gmail.com>
2016-08-06 12:40:24 +0200

    Fix bug on image. Change the interface : simple function. Add multiplpe way to get pokemon(s).

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const fs       = require('fs'),
+      leftPad  = require('left-pad'),
       pokemons = require('./pokemons');
 
-exports.findPokemonById = function(id) {
-  const nId = parseInt(id, 10) - 1;
+module.exports = function(id) {
+  if (Array.isArray(id)) {
+    return id.map(_getPokemonById);
+  }
+  if (1 < arguments.length) {
+    return Array.from(arguments).map(_getPokemonById);
+  }
+  return _getPokemonById(id);
+}
+
+
+function _getPokemonById(id) {
+  const nId = parseInt(id, 10) - 1,
+        sId = leftPad(id, 3, 0);
+
   return Object.assign({}, pokemons.pokemons[nId], {
-    image : new Buffer(fs.readFileSync('./img/' + id + '.png')).toString('base64')
+    image : new Buffer(fs.readFileSync('./img/' + sId + '.png')).toString('base64')
   });
 }
--- test/index.js
+++ test/index.js
@@ -1,15 +1,33 @@
 const fs = require('fs');
 const pokedex = require('..');
 
-const pokemon = pokedex.findPokemonById(4)
+const salameche   = pokedex(4);
+const pokemonsArr = pokedex([1, 7, 10, 13]);
+const pokemonsArg = pokedex('80', '056', 23);
+const pokemons  = [salameche].concat(pokemonsArr).concat(pokemonsArg);
 
 fs.writeFile('test.html',
-'<html>'
-+ '<body>'
-+ '<h1>' + pokemon.nameFr + '</h1>'
-+ '<img alt="' + pokemon.nameFr + '" src="data:image/png;base64, ' + pokemon.image+ '"/>'
-+ '</body>'
-+ '</html>'
+`
+<html>
+  <body>
+    <ul>
+      ${pokemons.map(pokemon => `
+        <li>
+          <ul>
+            <li>id     : ${pokemon.id}</li>
+            <li>nameFr : ${pokemon.nameFr}</li>
+            <li>nameEn : ${pokemon.nameEn}</li>
+            <li>nameDe : ${pokemon.nameDe}</li>
+            <li>nameJp : ${pokemon.nameJp}</li>
+            <li><img alt="${pokemon.nameFr}" src="data:image/png;base64, ${pokemon.image}"/></li>
+          </ul>
+        </li>
+        `)
+      }
+    </ul>
+  </body>'
+</html>
+`
 , err => {
   if (err) throw err;
   console.log('File generate');
